defdf1ba31d34e22355d2df432fc7e6d62ba71e4
ardimahensa <ardimahensa11@gmail.com>
2022-06-10 03:27:55 +0700

    crud

---
+++ lib/page/setting.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+//warna
+mixin ColorPalette {
+  static const Color aquaHaze = Color(0xffeff5f4);
+  static const Color pacificBlue = Color(0xff00b7b7);
+  static const Color white = Color(0xffffffff);
+  static const Color mandy = Color(0xffe75757);
+  static const Color timberGreen = Color(0xff1a2e35);
+  static const Color nileBlue = Color(0xff1a535c);
+  static const Color bondyBlue = Color(0xff0096a5);
+  static const Color geyser = Color(0xffd5e2e3);
+}
+
+//notif logout
+void showConfirmDialog(
+  BuildContext context,
+  String title,
+  String action1title,
+  String action2title,
+  Function action1,
+  Function action2,
+) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        elevation: 5,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontFamily: "Nunito",
+                fontSize: 22,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    action1();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 20.0, top: 12),
+                    child: Text(
+                      action1title,
+                      style: const TextStyle(
+                        color: Colors.red,
+                        fontFamily: "Nunito",
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    action2();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0, top: 12),
+                    child: Text(
+                      action2title,
+                      style: const TextStyle(
+                        color: Colors.green,
+                        fontFamily: "Nunito",
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,41 +1,266 @@
+// ignore_for_file: unnecessary_nullable_for_final_variable_declarations, use_build_context_synchronously, prefer_interpolation_to_compose_strings
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:sigb/page/setting.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key, String? name}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  //user auth
   final user = FirebaseAuth.instance.currentUser!;
+  // text controller
+  final TextEditingController _produkController = TextEditingController();
+  final TextEditingController _stokController = TextEditingController();
+  final TextEditingController _beratController = TextEditingController();
+  final TextEditingController _hargaController = TextEditingController();
+  //Sambungan ke database firestore
+  final CollectionReference _gabah =
+      FirebaseFirestore.instance.collection('gabah');
+
+  //Trigger untuk floating action button
+  Future<void> _createOrUpdate([DocumentSnapshot? documentSnapshot]) async {
+    String action = 'create';
+    if (documentSnapshot != null) {
+      action = 'update';
+      _produkController.text = documentSnapshot['produk'];
+      _stokController.text = documentSnapshot['stok'].toString();
+      _beratController.text = documentSnapshot['berat'];
+      _hargaController.text = documentSnapshot['harga'].toString();
+    }
+
+    await showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext ctx) {
+        return Padding(
+          padding: EdgeInsets.only(
+              top: 20,
+              left: 20,
+              right: 20,
+              //Input Textfield
+              bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: _produkController,
+                decoration: const InputDecoration(
+                  labelText: 'Nama Produk',
+                ),
+              ),
+              TextField(
+                controller: _stokController,
+                decoration: const InputDecoration(
+                  labelText: 'Banyak Stok',
+                ),
+              ),
+              TextField(
+                controller: _beratController,
+                decoration: const InputDecoration(
+                  labelText: 'Berat Untuk Stok',
+                ),
+              ),
+              TextField(
+                controller: _hargaController,
+                decoration: const InputDecoration(
+                  labelText: 'Harga per Stok',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+
+              //Fungsi untuk Create dan Update
+              ElevatedButton(
+                child: Text(action == 'create' ? 'Create' : 'Update'),
+                onPressed: () async {
+                  final String? produk = _produkController.text;
+                  final int? stok = int.tryParse(_stokController.text);
+                  final String? berat = _beratController.text;
+                  final int? harga = int.tryParse(_hargaController.text);
+                  if (produk != null &&
+                      stok != null &&
+                      berat != null &&
+                      harga != null) {
+                    if (action == 'create') {
+                      // Mengirim ke collection
+                      await _gabah.add({
+                        "produk": produk,
+                        "stok": stok,
+                        "berat": berat,
+                        "harga": harga
+                      });
+                    }
+
+                    if (action == 'update') {
+                      // Perintah update
+                      await _gabah.doc(documentSnapshot!.id).update({
+                        "produk": produk,
+                        "stok": stok,
+                        "berat": berat,
+                        "harga": harga
+                      });
+                    }
+
+                    // Menghapus textfield
+                    _produkController.text = '';
+                    _stokController.text = '';
+                    _beratController.text = '';
+                    _hargaController.text = '';
+
+                    // Menghilangkan bottom sheet
+                    Navigator.of(context).pop();
+                  }
+                },
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Fungsi untuk delete
+  Future<void> _deleteProduct(String gabahId) async {
+    await _gabah.doc(gabahId).delete();
+
+    // Tampulan jika data terhapus
+    ScaffoldMessenger.of(context)
+        .showSnackBar(const SnackBar(content: Text('Data berhasil dihapus')));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Home Page', style: TextStyle(color: Colors.white)),
         backgroundColor: const Color.fromARGB(229, 236, 174, 40),
         actions: [
-          MaterialButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: const Icon(
-              Icons.logout,
-              color: Color.fromARGB(255, 255, 255, 255),
+          IconButton(
+            splashColor: Colors.white,
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
             ),
-          )
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const HomePage(),
+                ),
+              );
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.logout_outlined,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              showConfirmDialog(
+                  context, "Yakin mau Logout ?", "Nanti deh", "Iya", () {
+                Navigator.of(context).pop();
+              }, () {
+                Navigator.of(context).pop();
+                FirebaseAuth.instance.signOut();
+              });
+            },
+          ),
         ],
       ),
       backgroundColor: Colors.grey,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Menu Awal ${user.email!}'),
-          ],
+      body: SafeArea(
+        child: StreamBuilder(
+          stream: _gabah.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      streamSnapshot.data!.docs[index];
+                  return Card(
+                    margin: const EdgeInsets.all(15),
+                    child: ListTile(
+                      title: Text(
+                        documentSnapshot['produk'],
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w500),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        // alignment: Alignment.bottomLeft,
+                        child: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Stok Tersedia : ' +
+                                      documentSnapshot['stok'].toString() +
+                                      ' ' +
+                                      documentSnapshot['berat'],
+                                  style: const TextStyle(color: Colors.red),
+                                  textAlign: TextAlign.left,
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  'Harga : Rp' +
+                                      documentSnapshot['harga'].toString() +
+                                      '/' +
+                                      documentSnapshot['berat'],
+                                  style: const TextStyle(color: Colors.green),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      // dense: true,
+                      isThreeLine: true,
+                      trailing: SizedBox(
+                        width: 96,
+                        child: Row(
+                          children: [
+                            //Icon untuk Update
+                            IconButton(
+                              icon: const Icon(Icons.edit),
+                              onPressed: () =>
+                                  _createOrUpdate(documentSnapshot),
+                              color: Colors.blueAccent,
+                            ),
+                            //Icon untuk Delete
+                            IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () =>
+                                    _deleteProduct(documentSnapshot.id),
+                                color: Colors.redAccent),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _createOrUpdate(),
+        backgroundColor: const Color.fromARGB(229, 236, 174, 40),
+        child: const Icon(Icons.add_outlined),
       ),
     );
   }
